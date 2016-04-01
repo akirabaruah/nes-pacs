1e7a9536a302f516f2dcfe2644a257a4c48d9c03
Sean <seanliu15@gmail.com>
2016-03-31 20:53:11 -0400

    formatting

--- src/alu.sv
+++ src/alu.sv
@@ -1,31 +1,28 @@
 module alu (input [7:0] alu_a,
-           input [7:0] alu_b,
-		   input [2:0] mode,
-		   input carry_in,
-		   output [7:0] alu_out,
-		   output carry_out,
-		   output overflow);
+            input [7:0] alu_b, 
+	    input [2:0] mode,
+	    input carry_in,
+	    output [7:0] alu_out,
+	    output carry_out,
+    	    output overflow);
 
    assign carry_out = 0;
    assign overflow = 0;
 
    parameter
       ALU_ADD = 3'd0,
-	  ALU_AND = 3'd1,
-	  ALU_OR  = 3'd2,
-	  ALU_XOR = 3'd3,
-	  ALU_SRS = 3'd4
+      ALU_AND = 3'd1,
+      ALU_OR  = 3'd2,
+      ALU_XOR = 3'd3,
+      ALU_SRS = 3'd4
    ;
    
    always_comb begin
-	  case (mode)
+      case (mode)
          ALU_ADD: alu_out = alu_a + alu_b;
-		 default: alu_out = alu_a + alu_b;
-	  endcase
+	 default: alu_out = alu_a + alu_b;
+      endcase
    end
 
-   
 endmodule
-
-
 
