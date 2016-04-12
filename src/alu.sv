ff917c637d215b28198f2749c0cf19198b1620ad
Sean <seanliu15@gmail.com>
2016-04-12 14:35:28 -0400

    added logic for overflow and carry in alu

--- src/alu.sv
+++ src/alu.sv
@@ -6,21 +6,31 @@ module alu (input [7:0] alu_a,
 	    output carry_out,
     	    output overflow);
 
-   assign carry_out = 0;
-   assign overflow = 0;
+//   assign carry_out = 0;
+//   assign verflow = 0;
 
-   if (AND == 001) 
-   assign carry_out = 1;
+//   if (AND == 00101) 
+//     carry_out = 1;
    
    
    always_comb begin
       case (mode)
-         ADC: alu_out = alu_a + alu_b; 
+	 ADC: begin 
+	      alu_out = alu_a + alu_b; 
+	      if (alu_a > 0 && alu_b > 0) 
+                 overflow = alu_out < alu_a;
+	      else if (alu_a < 0 && alu_b < 0)
+                 overflow = alu_out > alu_a; 
+      	      end
 	 AND: alu_out = alu_a & alu_b;
-	 ORA:  alu_out = alu_a | alu_b;
+	 ORA: alu_out = alu_a | alu_b;
 	 EOR: alu_out = alu_a ^ alu_b;
-//	 SRS: alu_out = alu_b >> 1;    // Shifting always uses alu_b register?? 
-	 default:  alu_out = alu_a + alu_b; 
+// 	 no SRS code yet
+//	 SRS: begin 
+//	      carry_out = alu_a[7]; 
+// 	      alu_out = alu_a >> 1; 
+//	      end    // Shifting always uses alu_a register?? 
+	 default: alu_out = alu_a; 
       endcase
    end
 
