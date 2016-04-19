c3ba51c3b12b3e5c6cd0e171072fcb2c44e98e8a
chaiwen <cc3636@columbia.edu>
2016-04-19 14:50:06 -0400

    removed alu old comments

--- src/alu.sv
+++ src/alu.sv
@@ -11,42 +11,23 @@ module alu (input [7:0] alu_a,
 				output sign);
    
 	logic [8:0] tmp_out; //9 bit add for easy overflow/carry checks
-	
-
+	logic carry_in_temp = carry_in; // maybe need this for 2's complement sub	
+		
+	// for right shift, alu_b will be zero
 	assign   alu_out = tmp_out[7:0];
 	assign   overflow = alu_a[7] ^ alu_b[7] ^ tmp_out[7];
 	assign	carry_out = tmp_out[8];
 
 	always_comb begin
 		case (mode)
-			ALU_ADD: begin tmp_out = alu_a + alu_b + carry_in; $display("added"); end
-			ALU_SUB: tmp_out = alu_a + ~alu_b + carry_in;
-			ALU_AND: begin tmp_out = alu_a & alu_b; $display("anded"); end
+			ALU_ADD: begin tmp_out = alu_a + alu_b + carry_in_temp; $display("alu_add"); end
+			ALU_SUB: begin tmp_out = alu_a + ~alu_b + carry_in_temp; $display("alu_sub"); end
+			ALU_AND: begin tmp_out = alu_a & alu_b; $display("alu_and"); end
 	 		ALU_OR : tmp_out = alu_a | alu_b;
 	 		ALU_EOR: tmp_out = alu_a ^ alu_b;
-	 		ALU_SR : tmp_out = {alu_a[0], carry_in, alu_a[7:1]};
-
-/*
-         // TODO: verify direction/orientation	      
-	 ASL: begin
-	      carry_out = alu_a[0];
-	      alu_out = alu_a << 1;
-	      end
-	 ROL: begin
-	      carry_out = alu_a[0];
-	      alu_out = alu_a << 1;
-	      alu_out[7] = carry_in;
-	      end
-	 ALU_SHR: begin
-	      carry_out = alu_a[7];
-              alu_out = alu_a >> 1;
-	      end
-	 ROR: begin
-	      carry_out = alu_a[7];
-	      alu_out = alu_a >> 1;
-	      alu_out[0] = carry_in;
-	      end
-*/
+	 		ALU_SR : begin
+							tmp_out = {alu_a[0], carry_in, alu_a[7:1]};
+						end //ASL,
 
 			default tmp_out = alu_a; 
 		endcase
