75bc4730ca75174d283cd01e6b04399322e518b5
Philip Schiffrin <philip.schiffrin@gmail.com>
2016-04-26 15:17:56 -0400

    don't trust this commit, lots broken, including eor

--- src/tests/cpu_tb.cpp
+++ src/tests/cpu_tb.cpp
@@ -19,6 +19,7 @@ int main(int argc, char **argv) {
 
     Vcpu *cpu = new Vcpu;
     char memory[MEMSIZE];
+	 memset((char *)memory, 9, sizeof(memory));
     int time = 0;
     uint16_t addr = 0;
     uint8_t input;
@@ -26,29 +27,32 @@ int main(int argc, char **argv) {
     /* Read assembled binary into simulated memory */
     FILE *binary = fopen(argv[1], "r");
     size_t len = fread(memory, 1, MEMSIZE, binary);
+	 printf("len = %zd\n", len);
 
 	printf("%8s,%8s,%8s,%8s\n",
 		   "time", "in", "out", "addr");
 
 	while (1) {
-        if (Verilated::gotFinish()) { break; }
+		if (Verilated::gotFinish()) { break; }
 
-        addr = cpu->addr;
-		if (addr >= len) break;
+		addr = cpu->addr;
 
-        input = memory[addr];
-        cpu->d_in = input;
+		if (addr == (len+1)) { break; }
 
-        printf("%8d,%8.2x,%8.2x,%8.2x\n",
-               time, cpu->d_in, cpu->d_out, cpu->addr);
+		input = memory[addr];
+		cpu->d_in = input; 
 
-        tick(cpu);
+
+		printf("%8d,%8.2x,%8.2x,%8.2x\n",
+				time, cpu->d_in, cpu->d_out, cpu->addr);
+
+		tick(cpu);
 		time++;
-    }
-    cpu->final();
+	}
+	cpu->final();
 
-    delete cpu;
-    return 0;
+	delete cpu;
+	return 0;
 }
 
 void tick(Vcpu *cpu) {
