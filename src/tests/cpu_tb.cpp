8c812587a5bfe011d858eb7422dbcbbfe110028a
Akira Baruah <akira.baruah@gmail.com>
2016-05-07 21:14:59 -0400

    cpu_tb.cpp: update printing

--- src/tests/cpu_tb.cpp
+++ src/tests/cpu_tb.cpp
@@ -7,6 +7,7 @@ using namespace std;
 #define MEMSIZE 65536
 
 void tick(Vcpu *cpu);
+void print_stats(Vcpu *cpu, int time);
 
 int main(int argc, char **argv) {
 
@@ -33,9 +34,6 @@ int main(int argc, char **argv) {
     }
     size_t len = fread(memory, 1, MEMSIZE, binary);
 
-	printf("%8s,%8s,%8s,%8s\n",
-		   "time", "in", "out", "addr");
-
 	while (1) {
 		if (Verilated::gotFinish()) { break; }
 
@@ -50,8 +48,7 @@ int main(int argc, char **argv) {
 		input = memory[addr];
 		cpu->d_in = input;
 
-		printf("%8d,%8.2x,%8.2x,%8.2x\n",
-               time, cpu->d_in, cpu->d_out, cpu->addr);
+        print_stats(cpu, time);
 		time++;
 	}
 	cpu->final();
@@ -65,4 +62,18 @@ void tick(Vcpu *cpu) {
     cpu->eval();
     cpu->clk = 1;
     cpu->eval();
+}
+
+void print_stats(Vcpu *cpu, int time) {
+    static int first = 1;
+    if (first) {
+        printf("%5s %2s %3s %4s\n",
+               "Cycle", "Op", "out", "addr");
+        first = 0;
+    }
+
+    if (cpu->sync) {
+        printf("%5d %.2x %.3x %.4x\n",
+               time, cpu->d_in, cpu->d_out, cpu->addr);
+    }
 }
