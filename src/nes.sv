1dc90aae7d3515494b1e4867db54205fb64ff3e6
chaiwen <cc3636@columbia.edu>
2016-05-09 18:50:42 -0400

    added qsys quartus files, this may not be good...

--- src/nes.sv
+++ src/nes.sv
@@ -1,9 +1,9 @@
 parameter
-   RESET_CPU  = 8'b0,
-   START_CPU = 8'b1,
-   START_WRITE = 8'b2,
-   WRITE = 8'b3,
-   STOP_WRITE = 8'b4
+   RESET_CPU  = 8'd0,
+   START_CPU = 8'd1,
+   START_WRITE = 8'd2,
+   WRITE = 8'd3,
+   STOP_WRITE = 8'd4
 ;
 
 module nes (
@@ -11,10 +11,27 @@ module nes (
    input logic reset,
    input logic chipselect,
    input logic read, 
-   output logic [7:0] readdata,
+   output logic [15:0] readdata,
    input logic write,
    input logic [15:0] writedata,
-   input logic [15:0] address
+   input logic [15:0] address,
+	
+	 output [14:0] hps_memory_mem_a,
+    output [2:0]  hps_memory_mem_ba,
+    output        hps_memory_mem_ck,
+    output        hps_memory_mem_ck_n,
+    output        hps_memory_mem_cke,
+    output        hps_memory_mem_cs_n,
+    output        hps_memory_mem_ras_n,
+    output        hps_memory_mem_cas_n,
+    output        hps_memory_mem_we_n,
+    output        hps_memory_mem_reset_n,
+    inout  [39:0] hps_memory_mem_dq,
+    inout  [4:0]  hps_memory_mem_dqs,
+    inout  [4:0]  hps_memory_mem_dqs_n,
+    output        hps_memory_mem_odt,
+    output [4:0]  hps_memory_mem_dm,
+    input         hps_memory_oct_rzqin
 );
 
 logic zero = 0;
@@ -71,7 +88,7 @@ always_ff @(posedge clk) begin
          mem_write <= 1;
          mem_addr <= mem_addr + 1;
          program_end <= program_end + 1;
-         mem_in <= writedata[7:0]
+         mem_in <= writedata[7:0];
          end
       STOP_WRITE: begin
          mem_write <= 0;
@@ -82,5 +99,27 @@ always_ff @(posedge clk) begin
          end
    endcase
 end
+
+soc_system soc (
+    .memory_mem_a        (hps_memory_mem_a),
+    .memory_mem_ba       (hps_memory_mem_ba),
+    .memory_mem_ck       (hps_memory_mem_ck),
+    .memory_mem_ck_n     (hps_memory_mem_ck_n),
+    .memory_mem_cke      (hps_memory_mem_cke),
+    .memory_mem_cs_n     (hps_memory_mem_cs_n),
+    .memory_mem_ras_n    (hps_memory_mem_ras_n),
+    .memory_mem_cas_n    (hps_memory_mem_cas_n),
+    .memory_mem_we_n     (hps_memory_mem_we_n),
+    .memory_mem_reset_n  (hps_memory_mem_reset_n),
+    .memory_mem_dq       (hps_memory_mem_dq),
+    .memory_mem_dqs      (hps_memory_mem_dqs),
+    .memory_mem_dqs_n    (hps_memory_mem_dqs_n),
+    .memory_mem_odt      (hps_memory_mem_odt),
+    .memory_mem_dm       (hps_memory_mem_dm),
+    .memory_oct_rzqin    (hps_memory_oct_rzqin),
+
+    .clk_clk (main_clk),
+    .reset_reset_n (!cpu_reset)
+);
 
 endmodule
