5e9b96f5122cf74329d4a6c31db344ea641bf8d7
Sean Liu <seanliu@dyn-160-39-148-174.dyn.columbia.edu>
2016-05-09 00:57:55 -0400

    whats going on

--- src/nes.sv
+++ src/nes.sv
@@ -1,7 +1,9 @@
 parameter
-   HALT  = 8'b0,
-   START = 8'b1,
-   WRITE = 8'b2
+   RESET_CPU  = 8'b0,
+   START_CPU = 8'b1,
+   START_WRITE = 8'b2,
+   WRITE = 8'b3,
+   STOP_WRITE = 8'b4
 ;
 
 module nes (
@@ -19,16 +21,17 @@ logic zero = 0;
 logic one = 1;
 logic cpu_ready;
 logic sync;
-logic cpu_write, mem_write;
+logic cpu_write, mem_write, cpu_reset;
 logic [15:0] cpu_addr, mem_addr;
 logic [7:0] d_in, d_out, mem_in, mem_out;
+logic [15:0] program_end = 0;
 
 logic [7:0] nes_op;                       // our own NES opcodes
 assign nes_op = writedata[15:8];
 
 cpu c (
    .clk (clk),
-   .reset (reset),
+   .reset (cpu_reset),
    .ready (cpu_ready),
    .irq (zero),
    .nmi (zero),
@@ -49,17 +52,34 @@ memory mem (
 
 always_ff @(posedge clk) begin
    case (nes_op)
-      START: cpu_ready <= 1;
-      STOP: begin
+      RESET_CPU: begin
          mem_write <= 0;
-         cpu_ready <= 0;
+         cpu_reset <= 1;
+         program_end <= 0;
+         end
+      START_CPU: begin
+         cpu_ready <= 1;
+         cpu_reset <= 0;
+         end
+      START_WRITE: begin
+         mem_addr <= address;
+         program_end <= address + 1;
+         mem_write <= 1;
+         mem_in <= writedata[7:0];
          end
       WRITE: begin
          mem_write <= 1;
-         mem_addr <= writedata[7:0];
+         mem_addr <= mem_addr + 1;
+         program_end <= program_end + 1;
+         mem_in <= writedata[7:0]
          end
-      default:
-         cpu_ready <= 1;
+      STOP_WRITE: begin
+         mem_write <= 0;
+         end
+      default: begin
+         mem_write <= 0;
+         cpu_ready <= 0;
+         end
    endcase
 end
 
