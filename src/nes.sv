c4867f1ca93ff2286b9100a9401e4b39500aee35
Sean Liu <seanliu@dyn-160-39-240-19.dyn.columbia.edu>
2016-05-09 18:25:23 -0400

    added more states

--- src/nes.sv
+++ src/nes.sv
@@ -1,22 +1,24 @@
 parameter
-   RESET_CPU  = 8'd0,
-   START_CPU  = 8'd1,
-   WRITE      = 8'd2,
+   RESET_CPU = 8'd0,
+   START_CPU = 8'd1,
+   PAUSE_CPU = 8'd2
+   WRITE_MEM = 8'd3,
 ;
 
 module nes (
    input logic clk,
-   input logic reset,                     // obsolete
-   input logic chipselect,                // ??
-   input logic read,                      // obsolete
-   output logic [7:0] readdata,
-   input logic write,                     // obsolete
+   input logic reset,               // obsolete
+   input logic chipselect,          // ??
+   input logic read,                // obsolete
+   input logic write,               // obsolete
    input logic [15:0] writedata,
-   input logic [15:0] address
+   input logic [15:0] address,
+   output logic [7:0] readdata
 );
 
 logic zero = 0;
 logic one = 1;
+
 logic cpu_ready;
 logic sync;
 logic cpu_write, mem_write, cpu_reset;
@@ -52,24 +54,38 @@ always_ff @(posedge clk) begin
    case (nes_op)
       RESET_CPU: begin
          mem_write <= 0;
+         cpu_ready <= 0;
          cpu_reset <= 1;
          end
+      WRITE_MEM: begin
+         mem_write <= 1;
+         cpu_ready <= 0;
+         mem_addr <= address;
+         mem_in <= d_in;
+         end
+      PAUSE_CPU: begin
+         mem_write <= 0;
+         cpu_ready <= 0;
+         cpu_reset <= 0;
+         end
       START_CPU: begin
+         mem_write <= 0;
          cpu_ready <= 1;
          cpu_reset <= 0;
          cpu_addr <= address;
          end
-      WRITE: begin
-         cpu_ready <= 0;
-         mem_write <= 1;
-         mem_addr <= addr;
-         mem_in <= writedata[7:0]
-         end
-      default: begin
+      default: begin             // keep running CPU
+         mem_write <= 0;
          cpu_ready <= 1;
-         cpu_addr <= cpu_addr + 1;
+         cpu_reset <= 0;
+         cpu_addr <= cpu_addr+1;
          end
    endcase
+
+   readdata <= d_out;
+
+   $display("nes_op:%d  d_in:%b  addr:%x  d_out:%b",
+      nes_op, d_in, addr, d_out);
 end
 
 
