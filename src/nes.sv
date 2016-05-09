86c1935ec5c211d06e41eaf3077d86cc774f49d2
chaiwen <cc3636@columbia.edu>
2016-05-08 23:24:55 -0400

    commiting broken psuedocode in nes.sv

--- src/nes.sv
+++ src/nes.sv
@@ -8,39 +8,55 @@ module nes (
 	output logic [7:0] readdata,
 
 	input logic write,
-	input logic [7:0] writedata,
+	input logic [15:0] writedata,
 	input logic [15:0] address
 );
 
-wire [7:0] nes_d_in;
-wire [7:0] nes_d_out;
-wire [7:0] nes_addr;
+logic zero = 0;
+logic one = 1;
+logic sync;
+logic cpu_write, mem_write;
+logic [15:0] cpu_addr, mem_addr;
+logic [7:0] d_in, d_out, mem_in, mem_out;
 
-wire main_clock = clk;
+
+logic [7:0] nes_op = writedata[15:8];
+
+if (writing to memory a binary)
+	memory.address = ?
+	memory.write = writedata[7:0];
+if stop
+if start
 
 cpu c (
-	.clk (main_clock),
-	.reset (),
-   .ready (),
-    .irq (),
-    .nmi (),
-     .d_in(),
-	  
-     .write(),
-     .sync(),
-     .d_out(),
-     .addr()
+	.clk (clk),
+	.reset (reset),
+   .ready (one),
+   .irq (zero),
+   .nmi (zero),
+ 
+   .d_in(d_in),
+   .write(cpu_write),
+
+   .sync(sync),
+
+   .d_out(d_out),
+   .addr(cpu_addr)
 );
 
-mixed_width_true_dual_port_ram_new_rw mem (
-		.addr1 (),
-		.addr2 (),
-		.data_in1 (), 
-		.data_in2 (), 
-		.we1 (), .we2 (),
-		.clk (main_clock),
-		
-		.data_out1 (),
-		.data_out2 ();
+memory mem (
+	.clk (clk),
+	.addr(mem_addr),
+	.write(mem_write),
+	.in(mem_in),
+	.out(mem_out)
 );
+
+always_ff @(posedge clk) begin
+
+
+
+end
+
+
 endmodule
