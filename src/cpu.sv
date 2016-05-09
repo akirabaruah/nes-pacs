4d797f2b80fa042806a284efae7e9acc2cdb8ba8
Akira Baruah <akira.baruah@gmail.com>
2016-05-08 21:42:28 -0400

    cpu.sv: carry in signal

--- src/cpu.sv
+++ src/cpu.sv
@@ -40,31 +40,17 @@ parameter
 
 
 module cpu (
-            input clk,
-            input reset,
-            input ready,
-            input irq,
-            input nmi,
-            input [7:0] d_in,
-            output write,
-            output sync,
-            output [7:0] d_out,
+            input         clk,
+            input         reset,
+            input         ready,
+            input         irq,
+            input         nmi,
+            input [7:0]   d_in,
+            output        write,
+            output        sync,
+            output [7:0]  d_out,
             output [15:0] addr
             );
-
-   /*
-    * Registers
-    */
-
-   logic [7:0] A,     // accumulator
-               X,     // X index
-               Y,     // Y index
-               D_OUT, // data output
-               IR,    // instruction register
-               P,     // processor status
-               SP,    // stack pointer
-               ADL,   // address low
-               ADH;   // address high
 
    /*
     * Instruction Fields
@@ -77,11 +63,41 @@ module cpu (
 
    assign {aaa, bbb, cc} = IR;
    assign opcode = {aaa, cc};
-	assign t1op = {d_in[7:5], d_in[1:0]};
+   assign t1op = {d_in[7:5], d_in[1:0]};
+
+   /*
+	* Arith control signal
+	*/
+
+   logic arith;
+   always_comb begin
+	  case (aaa)
+		LDA: 		arith = 0;
+
+		ORA:		arith = 1;
+		AND:		arith = 1;
+		EOR:		arith = 1;
+		ADC:		arith = 1;
+		SBC:		arith = 1;
+		default: arith = 0;
+	  endcase
+   end
 
 
+   /*
+    * Registers
+    */
 
-   logic [15:0] PC;   // program counter
+   logic [7:0]            A,     // accumulator
+                          X,     // X index
+                          Y,     // Y index
+                          D_OUT, // data output
+                          IR,    // instruction register
+                          P,     // processor status
+                          SP,    // stack pointer
+                          ADL,   // address low
+                          ADH;   // address high
+   logic [15:0]           PC;    // program counter
 
    /*
     * Instruction Register
@@ -93,96 +109,24 @@ module cpu (
           IR <= d_in;
      end
 
-
-	/*
-	 * Arith control signal
-	 */ 
-
-	logic arith;
-	always_comb begin
-		case (aaa)
-				LDA: 		arith = 0;
-
-				ORA:		arith = 1;
-				AND:		arith = 1;
-				EOR:		arith = 1;
-				ADC:		arith = 1;
-				SBC:		arith = 1;
-				default: arith = 0;
-		endcase
-	end
-			
-
-   /*
-    * Controller FSM
-    */
-
-   enum {
-         DECODE, // T0
-         FETCH,  // TX (final state of instruction)
-
-         ABS1,
-         ABS2,
-
-			ABSX1,
-			ABSX2,
-			ABSX3,
-
-			ZP1
-         } state;
-
-   initial state = FETCH;
-
-   always_ff @ (posedge clk)
-     begin
-        case (state)
-          FETCH:   state <= DECODE;
-          DECODE: begin
-             casex (d_in)
-               8'bxxx01101,
-               8'bxxx01110,
-               8'bxxx01100: state <= ABS1;  	 // Absolute
-					8'bxxx00101: state <= ZP1; 	 // Zero Page
-					8'bxxx11101: state <= ABSX1; // Absolute X
-               default:     state <= FETCH;   // Immediate
-             endcase
-          end
-
-          ABS1:    state <= ABS2;
-          ABS2:    state <= FETCH;
-
-			 ABSX1:	 state <= ABSX2;
-			 ABSX2:   state <= P[0] ? ABSX3 : FETCH;	
-			 ABSX3:	 state <= FETCH;
-			
-			 ZP1:		 state <= FETCH;
-
-          default: state <= FETCH;
-        endcase;
-
-        $display("sync:%b addr:%x d_in:%x A:%x X:%x Y:%x a:%x b:%x: out:%x P:%x",
-                 sync, addr, d_in, A, X, Y, alu_a, alu_b, alu_out, P);
-     end
-
-
    /*
     * Accumulator
     */
-//  ORA, AND, EOR, ADC, SBC
+   //  ORA, AND, EOR, ADC, SBC
 
    always_ff @ (posedge clk)
      begin
         case (state)
-			 DECODE:
-				case (aaa)
-					default: A <= A;
-				endcase
-			 FETCH:
-				if (arith)
-					A <= alu_out;
-				else
-					A <= d_in;
-			 default: 		A <= A;
+		  DECODE:
+			case (aaa)
+			  default: A <= A;
+			endcase
+		  FETCH:
+			if (arith)
+			  A <= alu_out;
+			else
+			  A <= d_in;
+		  default: 		A <= A;
         endcase;
      end
 
@@ -223,12 +167,15 @@ module cpu (
     * Program Counter
     */
 
-   logic pc_hold = 0;
    always_ff @ (posedge clk)
      begin
-        if (!pc_hold) begin
-           PC <= PC + 1;
-        end
+        case (state)
+		  ABS2,
+		  ABSX2,
+		  ABSX3,
+		  ZP1:     PC <= PC;
+		  default: PC <= PC + 1;
+        endcase
      end
 
    /*
@@ -240,9 +187,9 @@ module cpu (
         case (state)
           ABS1:    ADL <= d_in;
 
-			 ZP1:		 ADL <= d_in;
-	
-			 ABSX1:	 ADL <= alu_out;
+		  ZP1:		 ADL <= d_in;
+
+		  ABSX1:	 ADL <= alu_out;
 
           default: ADL <= 0;
         endcase;
@@ -257,13 +204,12 @@ module cpu (
         case (state)
           ABS2:    ADH <= d_in;
 
-			 ABSX2:	 ADH <= d_in;
-			 ABSX3:	 ADH <= alu_out;
+		  ABSX2:	 ADH <= d_in;
+		  ABSX3:	 ADH <= alu_out;
 
           default: ADH <= 0;
         endcase;
      end
-
 
    /*
     * Address Output
@@ -274,14 +220,68 @@ module cpu (
         case (state)
           ABS2:    addr = {d_in, ADL};
 
-			 ABSX2:	 addr = {d_in, ADL};
-			 ABSX3:	 addr = {ADH, ADL};
+		  ABSX2:	 addr = {d_in, ADL};
+		  ABSX3:	 addr = {ADH, ADL};
 
-			 ZP1:		 addr = {8'b0, d_in};
+		  ZP1:		 addr = {8'b0, d_in};
 
           default: addr = PC;
         endcase;
      end
+
+
+   /*
+    * Controller FSM
+    */
+
+   enum {
+         DECODE, // T0
+         FETCH,  // TX (final state of instruction)
+
+         ABS1,
+         ABS2,
+
+		 ABSX1,
+		 ABSX2,
+		 ABSX3,
+
+		 ZP1
+         } state;
+
+   initial state = FETCH;
+
+   always_ff @ (posedge clk)
+     begin
+        case (state)
+          FETCH:   state <= DECODE;
+          DECODE: begin
+             casex (state)
+               8'bxxx01101,
+               8'bxxx01110,
+               8'bxxx01100: state <= ABS1;  // Absolute
+               8'bxxx00101: state <= ZP1;   // Zero Page
+               8'bxxx11101: state <= ABSX1; // Absolute X
+               default:     state <= FETCH; // Immediate
+             endcase
+          end
+
+          ABS1:    state <= ABS2;
+          ABS2:    state <= FETCH;
+
+          ABSX1:   state <= ABSX2;
+          ABSX2:   state <= P[0] ? ABSX3 : FETCH;
+          ABSX3:   state <= FETCH;
+
+		  ZP1:     state <= FETCH;
+
+          default: state <= FETCH;
+        endcase;
+
+        $display("sync:%b addr:%x d_in:%x A:%x X:%x Y:%x a:%x b:%x: out:%x P:%x",
+                 sync, addr, d_in, A, X, Y, alu_a, alu_b, alu_out, P);
+     end
+
+
 
    /*
     * alu_a, alu_b control
@@ -291,11 +291,11 @@ module cpu (
      begin
         case (state)
 
-			 ABSX1:	 alu_a = dbus;
-			 ABSX3:	 alu_a = dbus;
+		  ABSX1:	 alu_a = dbus;
+		  ABSX3:	 alu_a = dbus;
 
           FETCH:   alu_a = dbus;
-			 
+
           default: alu_a = 0;
         endcase;
      end
@@ -304,14 +304,25 @@ module cpu (
      begin
         case (state)
 
-			 ABSX1:	 alu_b = d_in; // ADL
-			 ABSX3:	 alu_b = P[0];
+		  ABSX1:	 alu_b = d_in; // ADL
+		  ABSX3:	 alu_b = P[0];
 
-			 FETCH:	 alu_b = d_in;
+		  FETCH:	 alu_b = d_in;
           default: alu_b = d_in;
         endcase;
      end
 
+   /*
+    * ALU carry in
+    */
+
+   always_comb
+     begin
+        case (state)
+          ABSX3:   cin = P[0];
+          default: cin = 0;
+        endcase
+     end
 
    /*
     * Data Bus
@@ -322,33 +333,16 @@ module cpu (
      begin
 		case (state)
 
-			ABSX1: dbus = X;
-			ABSX3: dbus = ADH;
+		  ABSX1: dbus = X;
+		  ABSX3: dbus = ADH;
 
-			FETCH:
-				if (arith)
-					dbus = A; 
-				else
-					dbus = d_in;
+		  FETCH:
+			if (arith)
+			  dbus = A;
+			else
+			  dbus = d_in;
 		endcase
      end
-
-	/*
-	 * pc_hold signal
-	 */
-
-	always_comb
-		begin
-		 case (state)
-			ABS2: 	pc_hold = 1;
-
-			ABSX2:	pc_hold = 1;
-			ABSX3:	pc_hold = 1;
-			
-			ZP1:  	pc_hold = 1;
-			default: pc_hold = 0;
-		 endcase
-		end
 
    /*
     * ALU
@@ -356,12 +350,12 @@ module cpu (
 
    logic [7:0] alu_a, alu_b, alu_out;
    logic [4:0] alu_mode;
-   logic cout, over, zero, sign;
+   logic       cin, cout, over, zero, sign;
    alu ALU(
 		   .alu_a(alu_a),
 	       .alu_b(alu_b),
 		   .mode(alu_mode),
-	       .carry_in(0),
+	       .carry_in(cin),
 	       .alu_out(alu_out),
 	       .carry_out(cout),
 		   .overflow(over),
