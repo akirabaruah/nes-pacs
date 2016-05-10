6d792bf7af732b250f17596628a959ba514f5812
Sean Liu <seanliu@dyn-160-39-240-19.dyn.columbia.edu>
2016-05-10 00:01:46 -0400

    separated soc_system call from nes module

--- quartus/nes.sv
+++ quartus/nes.sv
@@ -13,24 +13,7 @@ module nes (
    input logic write,               // obsolete
    input logic [15:0] writedata,
    input logic [15:0] address,
-   output logic [15:0] readdata,
-	
-	output wire [12:0] memory_mem_a,       // memory.mem_a
-		output wire [2:0]  memory_mem_ba,      //       .mem_ba
-		output wire        memory_mem_ck,      //       .mem_ck
-		output wire        memory_mem_ck_n,    //       .mem_ck_n
-		output wire        memory_mem_cke,     //       .mem_cke
-		output wire        memory_mem_cs_n,    //       .mem_cs_n
-		output wire        memory_mem_ras_n,   //       .mem_ras_n
-		output wire        memory_mem_cas_n,   //       .mem_cas_n
-		output wire        memory_mem_we_n,    //       .mem_we_n
-		output wire        memory_mem_reset_n, //       .mem_reset_n
-		inout  wire [7:0]  memory_mem_dq,      //       .mem_dq
-		inout  wire        memory_mem_dqs,     //       .mem_dqs
-		inout  wire        memory_mem_dqs_n,   //       .mem_dqs_n
-		output wire        memory_mem_odt,     //       .mem_odt
-		output wire        memory_mem_dm,      //       .mem_dm
-		input  wire        memory_oct_rzqin    //       .oct_rzqin
+   output logic [15:0] readdata
 );
 
 logic zero = 0;
@@ -43,8 +26,8 @@ logic [15:0] cpu_addr, mem_addr, pc;
 logic [7:0] d_in, d_out, mem_in, mem_out;
 logic [7:0] nes_op;                       // our own NES opcodes
 
-//assign d_in = writedata[7:0];
-//assign nes_op = writedata[15:8];
+assign d_in = writedata[7:0];
+assign nes_op = writedata[15:8];
 
 cpu c (
    .clk (clk),
@@ -107,25 +90,5 @@ always_ff @(posedge clk) begin
    $display("nes_op:%d  d_in:%b  addr:%x  d_out:%b",
       nes_op, d_in, addr, d_out);
 end
-
-soc_system soc (
-	.clk (clk),
-	.memory_mem_a			(memory_mem_a),       // memory.mem_a
-	.memory_mem_ba			(memory_mem_ba),      //       .mem_ba
-	.memory_mem_ck			(memory_mem_ck),      //       .mem_ck
-	.memory_mem_ck_n		(memory_mem_ck_n),    //       .mem_ck_n
-	.memory_mem_cke		(memory_mem_cke),     //       .mem_cke
-	.memory_mem_cs_n		(memory_mem_cs_n),    //       .mem_cs_n
-	.memory_mem_ras_n		(memory_mem_ras_n),   //       .mem_ras_n
-	.memory_mem_cas_n		(memory_mem_cas_n),   //       .mem_cas_n
-	.memory_mem_we_n		(memory_mem_we_n),    //       .mem_we_n
-	.memory_mem_reset_n	(memory_mem_reset_n), //       .mem_reset_n
-	.memory_mem_dq			(memory_mem_dq),      //       .mem_dq
-	.memory_mem_dqs		(memory_mem_dqs),     //       .mem_dqs
-	.memory_mem_dqs_n		(memory_mem_dqs_n),   //       .mem_dqs_n
-	.memory_mem_odt		(memory_mem_odt),     //       .mem_odt
-	.memory_mem_dm			(memory_mem_dm),      //       .mem_dm
-	.memory_oct_rzqin		(memory_oct_rzqin)    //       .oct_rzqin
-);
 
 endmodule
